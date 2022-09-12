a16eb3f4a5acb4c8410a609f543595b5102ad1ee
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-13 02:45:40 +0300

    Initial commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-char alphabet;
+  char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-putchar(alphabet);
-}
+  {
+    putchar(alphabet);
+  }
 putchar('\n');
 return (0);
 }
