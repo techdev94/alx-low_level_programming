a2e4298285e538e0ef0e7434cee1b3abccfe3108
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-15 01:07:28 +0300

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int add(int, int) - adds two integers and returns the resul
+ *
+ * Return: Always 0.
+ */
+
+int add(int, int)
+{
+int result;
+result = int + int;
+
+_putchar(result);
+
+_putchar('\n');
+
+}
