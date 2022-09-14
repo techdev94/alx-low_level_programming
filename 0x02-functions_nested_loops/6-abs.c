04eb5dc7d5273d779d95f9f7841675f41ed05c95
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-15 00:50:09 +0300

    Initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _abs(int n) - retuns absoulute value
+ *
+ *n: The int to print
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (n);
+}
+}
