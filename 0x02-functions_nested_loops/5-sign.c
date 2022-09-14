698040583d8d425b1625f5846df2d8938e96c025
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-15 00:42:41 +0300

    Intial commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,16 +13,18 @@ int print_sign(int n)
 if (n >0)
 {
 return (1);
-printf("+")
+printf("+");
 }
 else if(n==0)
 {
 return (0);
-printf("0")
+printf("0");
 }
 else
 {
 return (-1);
-printf("-1")
+printf("-1");
 }
+  
+  return 0;
 }
