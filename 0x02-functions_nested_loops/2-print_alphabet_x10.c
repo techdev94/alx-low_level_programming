fa47af61ec35a1393e9d9b91d4c5df65cfacfd59
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-14 22:57:50 +0300

    Intial commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,14 @@ void print_alphabet_x10(void)
 {
 char alphabet;
 int count = 0;
-
+  
 for(count=0; count<10; count++)
 {
-  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-    {
-      _putchar(alphabet);
-    }
-  _putchar('\n');
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+ }
+  
+ _putchar('\n');
 }
 }
