07029bc29482309ed31c7a061c6a01a927c6a97b
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-14 13:32:35 +0300

    Initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <iostream>
+/**
+ * main - entry point of program
+ * Program prints _putchar followed by a newline
+ * Return: 0 (Success)
+ */
+
+int main()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+
+}
