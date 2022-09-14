cee8d909b3ace22f92a5f47b600beabd57c8094c
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-15 00:39:15 +0300

    Initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main -check code
+ * int print_sign(int n)- function that prints the sign of a number
+ *@c: The int to print
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+if (n >0)
+{
+return (1);
+printf("+")
+}
+else if(n==0)
+{
+return (0);
+printf("0")
+}
+else
+{
+return (-1);
+printf("-1")
+}
+}
