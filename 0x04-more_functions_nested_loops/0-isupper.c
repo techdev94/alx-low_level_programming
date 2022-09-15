b9f04c09cd2db880a44573f80fbe7a48c0deefb5
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-15 13:46:27 +0300

    Intial commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,14 +6,17 @@
  * Return: Always 0.
  */
  
- int _isupper(int c)
- {
- if (c > 'A' && c < 'Z')
- {
- return (1);
  
- } 
- else {
- return (0);
- } 
- }
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
