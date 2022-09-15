c93da5fe7433d5b4ec207c4f810db1f6f8973051
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-15 13:44:25 +0300

    Intial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character
+ * c: contains value to be compared
+ * Return: Always 0.
+ */
+ 
+ int _isupper(int c)
+ {
+ if (c > 'A' && c < 'Z')
+ {
+ return (1);
+ 
+ } 
+ else {
+ return (0);
+ } 
+ }
