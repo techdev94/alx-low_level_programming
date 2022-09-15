95d6250211bcce1187f6a56bd42e64ef9609866e
Michael Ngugi <111045011+techdev94@users.noreply.github.com>
2022-09-15 14:54:03 +0300

    Intial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int mul(int a, int b) - function that multiplies two integers
+ * a nd b contains integers to be multiplied
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+
+{
+return (a*b);
+}
